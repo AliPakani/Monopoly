b6b41e55403a85e50456cb59ddd4ac626c160dde
Mehdi <mehdi.shvp@gmail.com>
2025-12-25 00:33:35 +0330

    Menu Start

---
+++ menu1.py
@@ -0,0 +1,198 @@
+import json
+import os
+import re
+import uuid
+import bcrypt
+import keyboard  
+import time     
+
+FILE_PATH = "players.json"
+
+def load_players():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, "r") as f:
+            return json.load(f)
+    return []
+
+def save_players(players):
+    with open(FILE_PATH, "w") as f:
+        json.dump(players, f, indent=4)
+
+def is_valid_email(email):
+    pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+    return bool(re.match(pattern, email))
+
+def signup():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[1m༼ つ ◕_◕ ༽つ Sign Up\033[0m")
+    
+    players = load_players()
+
+    if len(players) >= 4:
+        print("Maximum player limit reached (4).")
+        input("Press Enter to continue...")
+        return
+
+    username = input("Enter username: ").strip()
+
+    while True:
+        email = input("Enter email: ").strip()
+        if is_valid_email(email):
+            break
+        print("Please enter a valid email")
+
+    while True:
+        password = input("Enter password: ").strip()
+        if len(password) >= 8:
+            break
+        print("Password must be at least 8 characters long.")
+
+    unique_id = str(uuid.uuid4())
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_bytes = bcrypt.hashpw(password_bytes, salt)
+    f_password = hashed_bytes.decode('utf-8')
+
+    new_player = {
+        "id": unique_id,
+        "username": username,
+        "email": email,
+        "password": f_password,
+        "position": 1,
+        "assest": 1500,
+        "ownership": []
+    }
+
+    players.append(new_player)
+    save_players(players)
+
+    print(f"Player {username} registered successfully!")
+    input("Press Enter to continue...") 
+
+def login():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[1m༼ つ ◕_◕ ༽つ LOGIN\033[0m")
+    
+    players = load_players() 
+
+    username_input = input("Enter username: ").strip()
+    password_input = input("Enter password: ").strip()
+
+    found_user = None
+    for player in players:
+        if player['username'] == username_input:
+            found_user = player
+            break
+
+    if found_user is None:
+        print("User not found!")
+        input("Press Enter to continue...")
+        return None  
+
+    input_bytes = password_input.encode('utf-8')
+    stored_hash_bytes = found_user['password'].encode('utf-8')
+
+    if bcrypt.checkpw(input_bytes, stored_hash_bytes):
+        print(f"Welcome back, {username_input}!")
+        time.sleep(0.5)
+        input("Press Enter to continue...")
+        return found_user 
+    else:
+        print("Incorrect password!")
+        input("Press Enter to continue...")
+        return None
+
+def interactive_menu(title, options):
+    
+    selected_index = 0
+    
+    time.sleep(0.2) 
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+        print(title)
+        print("")
+        for i, option in enumerate(options):
+            if i == selected_index:
+                print(f"> {option}") 
+            else:
+                print(f"   {option}")
+        
+        event = keyboard.read_event()
+
+        if event.event_type == keyboard.KEY_DOWN:
+            if event.name == 'up':
+                selected_index = (selected_index - 1) % len(options)
+            elif event.name == 'down':
+                selected_index = (selected_index + 1) % len(options)
+            elif event.name == 'enter':
+                return options[selected_index]
+            
+            time.sleep(0.2)
+
+def register_menu():
+    while True:
+        choice = interactive_menu(
+            "\033[1m༼ つ ◕_◕ ༽つ Register & Start\033[0m",
+            ["● Signup", "● Start" ,"● Back to Main Menu"]
+        )
+
+        if choice == "● Signup":
+            signup()
+        elif choice == "● Start":
+            print("main()")
+        elif choice == "● Back to Main Menu":
+            return 
+    
+def loadgame_menu():
+    while True:
+        choice = interactive_menu(
+            "\033[1m༼ つ ◕_◕ ༽つ Load Game\033[0m",
+            ["● Login","● Start", "● Back to Main Menu"]
+        )
+
+        if choice == "● Login":
+            login()
+        elif choice == "● Start":
+            print("main()")
+        elif choice == "● Back to Main Menu":
+            return
+
+def main_menu():
+    while True:
+        choice = interactive_menu(
+            "\033[1m༼ つ ◕_◕ ༽つ MONOPOLY\033[0m",
+            ["● New Game", "● Load Game", "● Leaderboard", "● Exit"]
+        )
+
+        if choice == "● New Game":
+            register_menu()
+        elif choice == "● Load Game":
+            loadgame_menu()
+        elif choice == "● Leaderboard":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Leaderboard selected (Not implemented yet)")
+            input("Press Enter to return...")
+        elif choice == "● Exit":
+            print("Good Bye")
+            break 
+def pause_menu():
+    
+    while True:
+        choice = interactive_menu(
+            f"\033[1m༼ つ ◕_◕ ༽つPause Menu\033[0m",
+            ["● Resume Game", "● Show Leaderboard", "● Save & Exit"]
+        )
+
+        if choice == "● Resume Game":
+            return "resume" 
+        elif choice == "● Show Leaderboard":
+            print("show_leaderboard")
+        elif choice == "● Save & Exit":
+            print("Saving game state...") 
+            time.sleep(1)
+            exit()
+
+pause_menu()

dc727c043ff07db5e72c215bf4aa9790bec8eb78
parmis <lppercyfallah@gmail.com>
2026-01-01 13:23:22 +0330

    Add leaderboard

--- menu1.py
+++ menu1.py
@@ -15,11 +15,14 @@ from time import sleep
 from rich.progress import track
 from rich.console import Console
 from rich.panel import Panel
+from rich.table import Table
+from rich import box
 console = Console()
 
 
 
-FILE_PATH = "players.json"
+CELLS = json.load(open("CELLS.json"))
+FILE_PATH = "PLAYERS.json"
 
 ready_players = []
 
@@ -187,6 +190,95 @@ def login():
         print("[red]Incorrect password![/red]")
         console.input("[bold yellow]Press Enter to continue...[/bold yellow]")
         return None
+    
+def Find_Price(Estate) :
+    Price = Estate["Price"]
+    return Price
+
+def Estate_value(Username) :                                   
+    Sum = 0
+    for Estate in Username["Estate"] :
+        Sum += Find_Price(Estate) 
+    return Sum 
+
+def Railroadـvalue(Username):
+    Sum = 0
+    for Railroad in Username["Railroad"] :
+        Sum += Find_Price(Railroad) 
+    return Sum 
+
+def company_value(Username):
+    Sum = 0
+    for Company in Username["Company"] :
+        Sum += Find_Price(Company) 
+    return Sum
+
+def total_value(Username):
+    return company_value(Username)+ Railroadـvalue(Username)+Estate_value(Username)+ Balance(Username)
+
+def Balance(Username):
+    value = Username.get("Balance", 0)
+    if value is None or value == "":
+        value = 0
+    return int(value)
+
+def Property_count(Username):
+    len_p = len(Username.get("Estate", [])) + len(Username.get("Company", [])) + len(Username.get("Railroad", []))
+    return len_p
+
+def estate_count(Username):
+    len_estate=len(Username.get("Estate", []))
+    return len_estate
+
+def Status(Username):
+    st= Username["Status"]
+    return st
+
+def score_leaderboard():
+    usernames = load_players()
+    lead_data=[]
+    for Username in usernames:
+        lead_data.append({
+            "Username": Username["Username"],
+            "Balance": Balance(Username),
+            "EstateCount": estate_count(Username),
+            "PropertyCount": Property_count(Username),
+            "EstateValue": Estate_value(Username),
+            "RailroadValue": Railroadـvalue(Username),
+            "CompanyValue": company_value(Username),
+            "TotalAssets": total_value(Username),
+            "Status": Status(Username),
+        })
+
+    lead_data.sort(key=lambda player: (player["TotalAssets"], player["PropertyCount"], player["Balance"]), reverse=True)
+
+    table = Table(title="[bold cyan]Leaderboard[/bold cyan]", box=box.ROUNDED)
+    table.add_column("Rank", justify="right",style="magenta" )
+    table.add_column("Username",style="cyan")
+    table.add_column("Balance", justify="right",style="magenta")
+    table.add_column("Estate Count", justify="right",style="magenta")
+    table.add_column("Estate Value", justify="right",style="magenta")
+    table.add_column("Railroad Value", justify="right",style="magenta")
+    table.add_column("Company Value", justify="right",style="magenta")
+    table.add_column("Total Assets", justify="right",style="magenta")
+    table.add_column("Status", style="cyan")
+
+    for index, data in enumerate(lead_data, start=1):
+        table.add_row(
+            str(index),
+            data["Username"],
+            str(data["Balance"]),
+            str(data["EstateCount"]),
+            str(data["EstateValue"]),
+            str(data["RailroadValue"]),
+            str(data["CompanyValue"]),
+            str(data["TotalAssets"]),
+            data["Status"]
+
+        )
+
+    console.print(table)
+    return lead_data
 
 def interactive_menu(title, options):
     
@@ -291,9 +383,7 @@ def main_menu():
         elif choice == "● Load Game":
             loadgame_menu()
         elif choice == "● Leaderboard":
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("Leaderboard selected (Not implemented yet)")
-            input("Press Enter to return...")
+            score_leaderboard()
         elif choice == "● Exit":
             ready_players.clear()
             print("All players logged out.")

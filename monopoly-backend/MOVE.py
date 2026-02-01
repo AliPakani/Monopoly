651637847137b389009a313d42cd36a85aacbbcd
Ali Pakani <98218107+AliPakani@users.noreply.github.com>
2026-02-01 14:35:19 +0330

    Delete monopoly-backend/MOVE.py

--- monopoly-backend/MOVE.py
+++
@@ -1,9 +0,0 @@
-from PLAYER import Deposit
-
-def Position(Username) :
-    try :
-        if Username["Position"] > 40 :
-            Username["Position"] %= 40
-            Deposit(Username, 200)
-    except Exception as e :
-        print(f"Error : {e}")

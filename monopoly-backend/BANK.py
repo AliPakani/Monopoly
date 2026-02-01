d3cf0d0c8edc6bc8d5c22085c9198d67029ea5fc
Ali Pakani <98218107+AliPakani@users.noreply.github.com>
2026-02-02 01:28:57 +0330

    Update BANK.py

--- monopoly-backend/BANK.py
+++ monopoly-backend/BANK.py
@@ -40,7 +40,7 @@ def Check_Rent(Username, Amount, CELLS):
             Debt = 0
             while Debt // 2 < Amount : 
                 Debt = Check_Assest(Username, AssestList)
-                print(("YOU ARE THE OWNER OF THESE ESTATES:", same_owner(Username, CELLS)).center(size))
+                print(f"YOU ARE THE OWNER OF THESE ESTATES: {same_owner(Username, CELLS)}".center(size))
                 EstateList = list(map(int, input(("Which estate do you want to sell?").center(size)).split()))
                 try :
                     AssestList = Find_Estate(EstateList)

5d680d1d417e6d34336c5180b66b51061577e71a
Ali Pakani <98218107+AliPakani@users.noreply.github.com>
2026-02-01 23:51:54 +0330

    Update JAIL.py

--- monopoly-backend/JAIL.py
+++ monopoly-backend/JAIL.py
@@ -6,7 +6,7 @@ size = os.get_terminal_size()
 size = size.columns
 
 
-def Jail(Username) :
+def Jail(Username, CELLS) :
     global size
     while True : 
         Choice = input("Do you want to pay 50$?(Y/N)".center(size)).lower().strip()
@@ -15,12 +15,12 @@ def Jail(Username) :
             continue
         first_character = Choice[0]
         if first_character == "y" :
-            Check_Rent(Username, 50)
+            Check_Rent(Username, 50, CELLS)
             return
         elif first_character == "n"  :
             Status = Roll_Jail(Username)
             if Status != 1 :
-                Check_Rent(Username, 50)
+                Check_Rent(Username, 50, CELLS)
             return
         else:
             print("Invalid input! TRY AGAIN".center(size))

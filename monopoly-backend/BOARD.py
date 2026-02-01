06c6d970b4f4de38c71829ef48805b12e1df8e42
lp-parmis <lppercyfallah@gmail.com>
2026-02-01 17:15:51 +0330

    Update BOARD.py

--- monopoly-backend/BOARD.py
+++ monopoly-backend/BOARD.py
@@ -1,32 +1,58 @@
 import os
 import time
-
+import rich
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from rich import box
+from rich.align import Align
+console = Console()
 
 size = os.get_terminal_size()
 size = size.columns
 
+Cell_style= {
+    "brown":     "bold black on #8B5A2B",
+    "cyen":      "bold black on #87CEFA",
+    "pink":      "bold black on #FF69B4",
+    "orange":    "bold black on #FFA500",
+    "red":       "bold white on #E53935",
+    "yellow":    "bold black on #FDD835",
+    "green":     "bold white on #43A047",
+    "blue":      "bold white on #1E88E5",
+}
+
+def cells_color(cell):
+    alias = cell["alias"]
+    c = cell["color"]              
+    c = c.strip().lower() 
+    style = Cell_style.get(c, "bold white on #2B2F36")
+    return f"[{style}]{alias}[/]"
+    
 def board_structure(Username, CELLS, PLAYERS) :
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(("╔" + 10 * "═════╦" + "═════╗").center(size))
+    console.print(Align.center("[yellow]╔[/yellow]" + 10 * "[yellow]═════╦[/yellow]" + "[yellow]═════╗[/yellow]"))
     #21 to 30
-    alias = "║"
-    position = "║"
+    alias = "[yellow]║[/yellow]"
+    position = "[yellow]║[/yellow]"
     for i in range(21, 32):
-        alias += " " + CELLS[i]["alias"] + " ║" 
+        alias += " " + cells_color(CELLS[i]) + " [yellow]║[/yellow]" 
         C1 = 1
         for j in PLAYERS : 
             if j["Position"] == i :
-                position += " " + "PL" + str(C1) + " ║"
+                position += " " + "[bold cyan]PL[/bold cyan]" + str(C1) + " [yellow]║[/yellow]"
                 break
             C1 += 1
         else  :
-            position += " " + "___" + " ║"
-    print((alias).center(size))
-    print((position).center(size))
-    print(("╠" + "═════╬" + 8 * "═════╩" + "═════╬" + "═════" + "╣").center(size))
+            position += " " + "___" + " [yellow]║[/yellow]"
+    console.print(Align.center(alias))        
+    console.print(Align.center(position))
+    console.print(Align.center("[yellow]╠[/yellow]" + "[yellow]═════╬[/yellow]" + 8 * "[yellow]═════╩[/yellow]" + "[yellow]═════╬[/yellow]" + "[yellow]═════[/yellow]" + "[yellow]╣[/yellow]"))
     #body
-    alias = "║"
-    position = "║"
+   
+    alias = "[yellow]║[/yellow]"
+    position = "[yellow]║[/yellow]"
     c = 12
     for i in range(20, 11, -1) :
         C1 = 1
@@ -48,38 +74,38 @@ def board_structure(Username, CELLS, PLAYERS) :
             player2 = "___"
             
         if i == 19 :
-            print(("║" + f" {CELLS[i]["alias"]} ║" + "GAME STATUS".center(53) + f"║ {CELLS[i + c]["alias"]} " + "║").center(size))
-            print(("║" + f" {player1} ║" + f"{PLAYERS[0]["Username"]}: {PLAYERS[0]["Balance"]}$".center(53) + f"║ {player2} " + "║").center(size))
-            print(("╠" + "═════╣" + f"{PLAYERS[1]["Username"]}: {PLAYERS[1]["Balance"]}$".center(53) + "╠═════" + "╣").center(size))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {cells_color(CELLS[i])} [yellow]║[/yellow]" + "GAME STATUS".center(53) + f"[yellow]║[/yellow] {cells_color(CELLS[i + c])} " + "[yellow]║[/yellow]"))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {player1} [yellow]║[/yellow]" + f"{PLAYERS[0]["Username"]}: {PLAYERS[0]["Balance"]}$".center(53) + f"[yellow]║[/yellow] {player2} " + "[yellow]║[/yellow]"))
+            console.print(Align.center("[yellow]╠[/yellow]" + "[yellow]═════╣[/yellow]" + f"{PLAYERS[1]["Username"]}: {PLAYERS[1]["Balance"]}$".center(53) + "[yellow]╠═════[/yellow]" + "[yellow]╣[/yellow]"))
         elif i == 18 :
-            print(("║" + f" {CELLS[i]["alias"]} ║" + f"{PLAYERS[2]["Username"]}: {PLAYERS[2]["Balance"]}$".center(53) + f"║ {CELLS[i + c]["alias"]} " + "║").center(size))
-            print(("║" + f" {player1} ║" + f"{PLAYERS[3]["Username"]}: {PLAYERS[3]["Balance"]}$".center(53) + f"║ {player2} " + "║").center(size))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {cells_color(CELLS[i])} [yellow]║[/yellow]" + f"{PLAYERS[2]["Username"]}: {PLAYERS[2]["Balance"]}$".center(53) + f"[yellow]║[/yellow] {cells_color(CELLS[i + c])} " + "[yellow]║[/yellow]"))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {player1} [yellow]║[/yellow]" + f"{PLAYERS[3]["Username"]}: {PLAYERS[3]["Balance"]}$".center(53) + f"[yellow]║[/yellow] {player2} " + "[yellow]║[/yellow]"))
         elif i == 16 :
-            print(("║" + f" {CELLS[i]["alias"]} ║" + f"It's {Username["Username"]}'s turn!".center(53) + f"║ {CELLS[i + c]["alias"]} " + "║").center(size))
-            print(("║" + f" {player1} ║" + " " * 53 + f"║ {player2} " + "║").center(size))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {cells_color(CELLS[i])} [yellow]║[/yellow]" + f"It's {Username["Username"]}'s turn!".center(53) + f"[yellow]║[/yellow] {cells_color(CELLS[i + c])} " + "[yellow]║[/yellow]"))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {player1} [yellow]║[/yellow]" + " " * 53 + f"[yellow]║[/yellow] {player2} " + "[yellow]║[/yellow]"))
         else :
-            print(("║" + f" {CELLS[i]["alias"]} ║" + " " * 53 + f"║ {CELLS[i + c]["alias"]} " + "║").center(size))
-            print(("║" + f" {player1} ║" + " " * 53 + f"║ {player2} " + "║").center(size))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {cells_color(CELLS[i])} [yellow]║[/yellow]" + " " * 53 + f"[yellow]║[/yellow] {cells_color(CELLS[i + c])} " + "[yellow]║[/yellow]"))
+            console.print(Align.center("[yellow]║[/yellow]" + f" {player1} [yellow]║[/yellow]" + " " * 53 + f"[yellow]║[/yellow] {player2} " + "[yellow]║[/yellow]"))
         if i != 12 and i != 19:
-            print(("╠" + "═════╣" + " " * 53 + "╠═════" + "╣").center(size))
+            console.print(Align.center("[yellow]╠[/yellow]" + "[yellow]═════╣[/yellow]" + " " * 53 + "[yellow]╠═════[/yellow]" + "[yellow]╣[/yellow]"))
         c += 2
     #1 to 11
-    print(("╠" + "═════╬" + 8 * "═════╦" + "═════╬" + "═════" + "╣").center(size))
-    alias = "║"
-    position = "║"
+    console.print(Align.center("[yellow]╠[/yellow]" + "[yellow]═════╬[/yellow]" + 8 * "[yellow]═════╦[/yellow]" + "[yellow]═════╬[/yellow]" + "[yellow]═════[/yellow]" + "[yellow]╣[/yellow]"))
+    alias = "[yellow]║[/yellow]"
+    position = "[yellow]║[/yellow]"
     for i in range(11, 0, -1):
-        alias += " " + CELLS[i]["alias"] + " ║" 
+        alias += " " + cells_color(CELLS[i]) + " [yellow]║[/yellow]"
         C1 = 1
         for j in PLAYERS : 
             if j["Position"] == i :
-                position += " " + "PL" + str(C1) + " ║"
+                position += " " + "PL" + str(C1) + " [yellow]║[/yellow]"
                 break
             C1 += 1
         else  :
-            position += " " + "___" + " ║"
-    print((alias).center(size))
-    print((position).center(size))
-    print(("╚" + 10 * "═════╩" + "═════╝").center(size))
+            position += " " + "___" + " [yellow]║[/yellow]"
+    console.print(Align.center(alias))
+    console.print(Align.center(position))
+    console.print(Align.center("[yellow]╚[/yellow]" + 10 * "[yellow]═════╩[/yellow]" + "[yellow]═════╝[/yellow]"))
     print()
 
 

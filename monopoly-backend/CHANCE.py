2851df24e3a9fae0d5f89f085bd2c7ea55437e92
Ali Pakani <98218107+AliPakani@users.noreply.github.com>
2026-02-01 14:25:05 +0330

    Cen

--- monopoly-backend/CHANCE.py
+++ monopoly-backend/CHANCE.py
@@ -1,52 +1,63 @@
 import random
 from MOVE import Position
-from PLAYER import Bankrupt, Dedute, Deposit, Buy, Sell, Assest, Go_To_Jail
-from ESTATE import Find_Owner, Find_Price, Find_Rent, Owner_Replace, Find_Estate
-from BANK import Check_Balance, Check_Rent
-from RAILROAD import Railroad
-from COMPANY import Company_Buy, Company_Rent
-import json
+from PLAYER import Deposit, Go_To_Jail
+from BANK import Check_Rent
+import os
+import time
+from BOARD import animated_move
 
 
-def Chance(Username, Number, PLAYERS):
+size = os.get_terminal_size()
+size = size.columns
+
+
+def Chance(Username, Number, PLAYERS, CELLS):
     Chance = [8, 23, 37]
     if Number in Chance :
         Random = random.randint(1,7)
         match Random :
             case 1:
-                print("Let's see what luck has for you...")
-                print("-->Advance to GO (collect $200)")
-                Username["Position"] = 41
-                Position(Username)
+                print("Let's see what luck has for you...".center(size))
+                print("-->Advance to GO (collect $200)".center(size))
+                time.sleep(2)
+                animated_move(Username, 41 - Username["Position"], CELLS, PLAYERS)
             case 2:
-                print("Let's see what luck has for you...")
-                print("-->Advance to Boardwalk")
-                Username["Position"] = 40
+                print("Let's see what luck has for you...".center(size))
+                print("-->Advance to Boardwalk".center(size))
+                time.sleep(2)
+                animated_move(Username, 40 - Username["Position"], CELLS, PLAYERS)
             case 3:
-                print("Let's see what luck has for you...")
-                print("-->Go to Jail")
-                Go_To_Jail(Username)
+                print("Let's see what luck has for you...".center(size))
+                print("-->Go to Jail".center(size))
+                time.sleep(2)
+                Go_To_Jail(Username, CELLS, PLAYERS)
             case 4:
-                print("Let's see what luck has for you...")
-                print("-->High-Risk investment! Roll the dice to see your fate")
+                print("Let's see what luck has for you...".center(size))
+                print("-->High-Risk investment! Roll the dice to see your fate".center(size))
+                time.sleep(2)
                 dice = random.randint(1,6)
                 if dice % 2 == 0 :
                     Deposit(Username, 300)
+                    time.sleep(2)
                 else :
                     Check_Rent(Username, 200)
+                    time.sleep(2)
             case 5 :
-                print("Let's see what luck has for you...")
-                print("-->Go Back 3 Spaces")
-                Username["Position"] -= 3
+                print("Let's see what luck has for you...".center(size))
+                print("-->Go Back 3 Spaces".center(size))
+                time.sleep(2)
+                animated_move(Username, -3, CELLS, PLAYERS)
             case 6 :
-                print("Let's see what luck has for you...")
-                print("-->Its Your Birthday!!Collect $50 and move forward 1 space")
+                print("Let's see what luck has for you...".center(size))
+                print("-->Its Your Birthday!!Collect $50 and move forward 1 space".center(size))
                 Username["Balance"] += 50
-                Username["Position"] += 1
+                time.sleep(2)
+                animated_move(Username, 1, CELLS, PLAYERS)
             case 7:
-                print("Let's see what luck has for you...")
-                print("-->You have been elected Chairman of the Board, Pay each player $50")
+                print("Let's see what luck has for you...".center(size))
+                print("-->You have been elected Chairman of the Board, Pay each player $50".center(size))
                 for player in PLAYERS:
                     if player != Username:
                         Check_Rent(Username, 50)
                         Deposit(player, 50)
+                        time.sleep(2)

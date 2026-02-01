c753a59c0c5da08cf0722e2f81bd99b806b8c1ca
Ali Pakani <98218107+AliPakani@users.noreply.github.com>
2026-02-01 14:31:11 +0330

    Centrize texts

--- monopoly-backend/DICE.py
+++ monopoly-backend/DICE.py
@@ -1,20 +1,23 @@
 import random
-from MOVE import Position
+import time
+import os
+
+size = os.get_terminal_size()
+size = size.columns
 
 def Roll(Username) :
-    Choice = input("Roll the dice")
+    Choice = input(("Roll the dice").center(size))
     c = 0
-    Move = 0
-    Dice1 = random.randint(1,6)
-    Dice2 = random.randint(1,6)
-    Move += Dice1 + Dice2
-    Username["Position"] += Move
+    Dice1 = int(input())
+    Dice2 = int(input())
+    Move = Dice1 + Dice2
     Username["Dice"] = [Dice1, Dice2]
-    print(f"You roll {Dice1} & {Dice2}")
+    print((f"You roll {Dice1} & {Dice2}").center(size))
+    time.sleep(2)
     if Dice1 != Dice2 :
-        return 0
+        return (Move, 0)
     else :
-        return 1
+        return (Move, 1)
 
 #Rolling a dice in JAIL
 def Roll_Jail(Username) :
@@ -26,7 +29,6 @@ def Roll_Jail(Username) :
         if Dice1 == Dice2:
             Username["Arrested"] = False
             Username["Position"] += Dice1 + Dice2
-            Position(Username)
             return 1
     return 0
         
